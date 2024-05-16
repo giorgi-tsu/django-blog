c63d9d8b74a2d7f09c4b387aa17137dee84fa737
giorgi-tsu <giorgi.tsu@gmail.com>
2024-05-16 21:34:25 +0400

    Add blog_index view to the views.py file

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from blog.models import Post, Comment
 
 # Create your views here.
+
+def blog_index(request):
+    posts = Post.objects.all().order_by("-created_on")
+    context = {
+        "posts": posts
+    }
+
+    return render(request, "blog/index.html", context)
+
+
+
