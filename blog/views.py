388288f7e8d45b7dd7e37d6a5068622df6793027
giorgi-tsu <giorgi.tsu@gmail.com>
2024-05-16 21:47:53 +0400

    Add blog_detail view to the views.py file

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,6 @@ def blog_index(request):
     context = {
         "posts": posts
     }
-
     return render(request, "blog/index.html", context)
 
 
@@ -20,8 +19,18 @@ def blog_category(request, category):
         "category": category,
         "posts": posts
     }
-
     return render(request, "blog/category.html", context)
+
+
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments
+    }
+    return render(request, "blog/detail.html", context)
+
 
 
 
